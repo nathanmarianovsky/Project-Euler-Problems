bf9d615a43703411bc6270e3a87645787e9d2d2f
nathanmarianovsky <Nathan@Nathan-Marianovskys-MacBook-Pro.local>
2015-07-20 15:37:22 -0400

    Problem 1
    A better implementation!

--- problem_001/problem_01.cpp
+++ problem_001/problem_01.cpp
@@ -1,15 +1,3 @@
-/*
-Project Euler Problem #1:
-	If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-	Find the sum of all the multiples of 3 or 5 below 1000.
-
-My Solution:
-	I extended this problem to a more general approach. The code below will find the sum of all the multiples of 3 or 5 below N.
-
-- Nathan Marianvosky
-*/
-
-
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -17,28 +5,20 @@ My Solution:
 using namespace std;
 
 
-//Find all natural numbers below N that are divisible by 3 or 5
-void findElements(vector<int> &array, int N) {
-	for(size_t i = 0; i < N; i++) {
-		if((remainder(i, 3) == 0) || (remainder(i, 5) == 0)) {
-			array.push_back(i);
-		}
-	}
-}
-
-
 //Computes the sum of all numbers divisible by either 3 or 5 but less than N
-int findSum(vector<int> &array) {
+int findSum(int N) {
 	int sum = 0;
-	if(array.size() == 0) {
-		return sum;
-	}
-	else {
-		for(size_t i = 0; i < array.size(); i++) {
-			sum += array[i];
+	int k1 = floor(N/3);
+	int k2 = floor(N/5);
+	for(int i = 1; i < k1; i++) {sum += 3*i;}
+	for(int j = 1; j < k2; j++) {
+		if(5*j % 3 != 0) {
+			sum += 5*j;
 		}
-	return sum;
 	}
+	if(k1 * 3 < N) {sum += 3*k1;}
+	if(k2 * 5 < N) {sum += 5*k2;}
+	return sum;
 }
 
 
@@ -80,8 +60,7 @@ int main() {
 	int sum = 0;
 	vector<int> array;
 	N = findN();
-	findElements(array, N);
-	sum = findSum(array);
+	sum = findSum(N);
 	printSum(N, sum);
 	return 0;
 }
