577e29dc966eb59efe35f0277544efa12dd01e93
nathanmarianovsky <Nathan@Nathan-Marianovskys-MacBook-Pro.local>
2015-07-20 20:50:19 -0400

    Formatting

--- problem_124/problem_124.cpp
+++ problem_124/problem_124.cpp
@@ -1,32 +1,3 @@
-/*
-Project Euler Problem #124:
-	The radical of n, rad(n), is the product of the distinct prime factors of n. For example, 504 = 23 × 32 × 7, so rad(504) = 2 × 3 × 7 = 42.
-	If we calculate rad(n) for 1 ≤ n ≤ 10, then sort them on rad(n), and sorting on n if the radical values are equal, we get:
-
-	Unsorted 		   Sorted
-	n  rad(n) 		n  rad(n)  k
-	1   1 			1    1 	   1
-	2   2 			2    2 	   2
-	3   3 			4    2 	   3
-	4   2 			8    2 	   4
-	5   5 			3    3 	   5
-	6   6			9    3 	   6
-	7   7 			5    5 	   7
-	8   2 			6    6 	   8
-	9   3 			7    7 	   9
-	10  10 			10   10    10
-
-	Let E(k) be the kth element in the sorted n column; for example, E(4) = 8 and E(6) = 9.
-	If rad(n) is sorted for 1 ≤ n ≤ 100000, find E(10000).
-
-My Solution:
-	I extended this problem to a more general approach. The code below will first produce a list of length N where N is given by the user.
-	Next the user can ask for the value of the kth position, essentially providing E(k).
-
-- Nathan Marianovsky
-*/
-
-
 #include <vector>
 #include <iostream>
 #include <string>
@@ -137,7 +108,7 @@ bool is_number(const string &s) {
 int findN() {
 	int N = 0;
 	string tmp;
-	cout << "This code will find any element produced in the rad(N) sorted list. What would you like the value of N to be?" << endl;
+	cout << "This code will find any element produced in the rad(k) sorted list. What would you like the value of N to be where N is the size of the list?" << endl;
 	cin >> tmp;
 	while(!(is_number(tmp)) || (stoi(tmp) == 0)) {
 		cout << "Please choose an integer value such that N > 0!" << endl;
